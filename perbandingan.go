ae594197d801e44b0d82103a569d28796f3c31d3
DimasAdityaGH <dimasubuntu21@gmail.com>
2021-06-27 13:57:24 +0700

    menambahkan file perbandingan

---
+++ perbandingan.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	var name1 = "eko"
+	var name2 = "budi"
+
+	var result = name1 == name2
+	fmt.Println(result)
+
+	var angka1 = 100
+	var angka2 = 200
+	fmt.Println(angka1 > angka2)
+	fmt.Println(angka1 < angka2)
+	fmt.Println(angka1 == angka2)
+	fmt.Println(angka1 != angka2)
+}
