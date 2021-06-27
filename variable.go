f842ba1da991d86da83dd43a98a39a71458a1e92
DimasAdityaGH <dimasubuntu21@gmail.com>
2021-06-27 10:21:50 +0700

    menambahkan file variable

---
+++ variable.go
@@ -0,0 +1,29 @@
+package main
+
+import "fmt"
+
+func main () {
+	var (
+		firstName = "dimas"
+		lastName = "aditya"
+	)
+	fmt.Println(firstName)
+	fmt.Println(lastName)
+
+	nama := "adit"
+	fmt.Println(nama)
+
+	nama = "tya"
+	fmt.Println(nama)
+
+	var kota string = "lamongan"
+	fmt.Println(kota)
+
+	var desa = "tejoasri"
+	fmt.Println(desa)
+
+	var sekolah string
+
+	sekolah = "smkn 1 kalitengah"
+	fmt.Println(sekolah)
+}
