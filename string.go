8f0470ceae2ec40145381096d1ee6ece7a5124c5
DimasAdityaGH <dimasubuntu21@gmail.com>
2021-06-27 10:16:44 +0700

    menambahkan file string

---
+++ string.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main () {
+	var nama = "dimas"
+	var umur = 17
+	var asal = "lamongan"
+	fmt.Println("nama: ", nama)
+	fmt.Println("umur: ", umur)
+	fmt.Println("asal: ", asal)
+
+}
