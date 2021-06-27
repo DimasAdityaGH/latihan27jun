8fa8ee514acb9ae3e6057be49af76f27ad0304d9
DimasAdityaGH <dimasubuntu21@gmail.com>
2021-06-27 10:38:49 +0700

    menmabahkan operasimatematika

---
+++ operasimatematika.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main () {
+	var a = 10
+	var b = 10
+	var c = a + b
+	fmt.Println(c)
+
+	var result = 101 * 2
+	fmt.Println(result)
+
+	var age = 17
+	age += 1
+	fmt.Println(age)
+
+	umur := 16
+	umur++
+	fmt.Println(umur)
+}
