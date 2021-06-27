83a5056f713c2a04a686191d912be7abbe8a1052
DimasAdityaGH <dimasubuntu21@gmail.com>
2021-06-27 10:26:44 +0700

    menambahkan file constant

---
+++ constant.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main () {
+	const (
+		namaDepan = "dwi"
+		namaBelakang = "reydesta"
+	)
+	fmt.Println(namaDepan)
+	fmt.Println(namaBelakang)
+
+	const nama = "dimas"
+	fmt.Println(nama)
+}
